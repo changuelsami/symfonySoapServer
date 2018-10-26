dd326f0056077079a576545c0a5607e7c6157fc8
sami.ch.27@gmail.com <sami.ch.27@gmail.com>
2018-10-26 09:34:37 +0100

    remove constructor, add comments

--- src/AppBundle/Controller/SoapController.php
+++ src/AppBundle/Controller/SoapController.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Create a simple HelloWorld SOAP server. The main function (hello) is defined in HelloController 
+ * which is a basic Controller. To publish a method of HelloController we've created this controller 
+ * (SoapController) where we have one + two methods : 
+ *  - First one  : how to publish all public methods of HelloController
+ *  - Second one : how to generate the WSDL of the service
+ *  - Third one  : how to call a method 
+ * 
+ * If you have a new controller to publish simply duplicate the first method and update these data : 
+ *  - The route and it's name
+ *  - The url
+ *  - The instance of the new controller
+ */
 
 namespace AppBundle\Controller;
 
@@ -7,43 +20,38 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// Needed to generate absolute URL
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+// Needed to create the SOAP server
 use Zend\Soap;
 
 class SoapController extends Controller
-{
+{  
     /**
-     * The base URL of the web service
-     * @var string
-     */
-    private $serverUrl;
-
-    /**
-     * Constructor
-     */
-    public function __construct() {
-        $this->serverUrl = "http://" . $_SERVER['HTTP_HOST'];
-    }
-    
-    /**
-     * To display the WSDL you have to call http://.../soap/hello?wsdl
+     * To display the WSDL you have to call http://.../soap/hello?wsdl (see @Route)
+     *
      * @Route("/soap/hello", name="soap_hello")
      */
     public function hello()
     {
-        $sevice = new HelloController();
-        if (isset($_GET['wsdl']))
-            return $this->handleWSDL($this->generateUrl('soap_hello'), $sevice);
-        else
-            return $this->handleSOAP($this->generateUrl('soap_hello'), $sevice);
-    }
+        // This wil generate the absolute URL of the current action (end point) based on it's route name
+        $theUri = $this->generateUrl('soap_hello', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        // This is the object to instanciate when the webservice is invoked, use any controller
+        $theService = new HelloController();
 
+        // Check if we should disply WSDL or execute the call
+        if (isset($_GET['wsdl']))
+            return $this->handleWSDL($theUri, $theService);
+        else
+            return $this->handleSOAP($theUri, $theService);
+    }
 
     /**
      * return the WSDL
      */
     public function handleWSDL($uri, $class)
     {
-        $uri = $this->serverUrl . $uri;
         // Soap auto discover
         $autodiscover = new Soap\AutoDiscover();
         $autodiscover->setClass($class);
@@ -51,11 +59,13 @@ class SoapController extends Controller
 
         // Response
         $response = new Response();
-        $response->headers->set('Content-Type', 'text/xml; charset=ISO-8859-1');
+        $response->headers->set('Content-Type', 'text/xml; charset=ISO-8859-1'); // WSDL is a XML content
+        
+        // Start Output Buffering, nothing will be displayed ...
         ob_start();
-
         // Handle Soap
         $autodiscover->handle();
+        // ... Stop Output Buffering and get content into variable
         $response->setContent(ob_get_clean());
         return $response;
     }
@@ -65,7 +75,6 @@ class SoapController extends Controller
      */
     public function handleSOAP($uri, $class)
     {
-        $uri = $this->serverUrl . $uri;
         // Soap server
         $soap = new Soap\Server(null,
             array('location' => $uri,
